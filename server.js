905c47240a44c75fedf51ca8a25c5b11d9a594f7
unknown <tannerwlumsden@gmail.com>
2023-01-12 18:25:13 -0500

    fixed links

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
+const fs = require('fs');
+const path = require('path');
+const { v4: uuid4 } = require('uuid');
+
+
 const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-res.send('Hello class');
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
-app.listen(PORT)
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+
+  
+  app.listen(PORT, () => console.log('running at port 3001'));
